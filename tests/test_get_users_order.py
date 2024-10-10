5a836a09f2ec1bd2153ac641bafcdb4a323bb3d5
MariyaMikhaylova <mikhailovams87@gmail.com>
2024-10-10 18:56:17 +0300

    Fix return -> assert

--- tests/test_get_users_order.py
+++ tests/test_get_users_order.py
@@ -9,7 +9,7 @@ class TestGetUsersOrder:
     def test_get_order_list_authorized(self, user_token, create_order_authorized):
         get_order_list = BurgerApi.get_users_order(user_token)
         response_body = get_order_list.json()
-        return (get_order_list.status_code == 200
+        assert (get_order_list.status_code == 200
                 and response_body["success"]
                 and response_body["orders"][0]["_id"] == create_order_authorized["order"]["_id"]
                 and response_body["total"] > 0
@@ -19,6 +19,6 @@ class TestGetUsersOrder:
     def test_get_order_list_unauthorized(self):
         get_order_list = BurgerApi.get_users_order('')
         response_body = get_order_list.json()
-        return (get_order_list.status_code == 401
+        assert (get_order_list.status_code == 401
                 and not response_body["success"]
                 and response_body["message"] == "You should be authorised")
